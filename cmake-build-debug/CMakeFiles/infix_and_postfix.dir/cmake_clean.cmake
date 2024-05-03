c141ee01191ac596c43a53eed3b4d5050072e35d
Karimnaggar4040 <150073117+Karimnaggar4040@users.noreply.github.com>
2024-05-04 01:36:06 +0300

    last

---
+++ cmake-build-debug/CMakeFiles/infix_and_postfix.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/infix_and_postfix.dir/project.c.obj"
+  "CMakeFiles/infix_and_postfix.dir/project.c.obj.d"
+  "infix_and_postfix.exe"
+  "infix_and_postfix.exe.manifest"
+  "infix_and_postfix.pdb"
+  "libinfix_and_postfix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/infix_and_postfix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/infix_and_postfix.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for infix_and_postfix.
+# This may be replaced when dependencies are built.
