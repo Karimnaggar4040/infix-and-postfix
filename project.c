aca63332d51b6f8971d18858ca3fe653d5616662
Karimnaggar4040 <150073117+Karimnaggar4040@users.noreply.github.com>
2024-05-04 19:38:21 +0300

    last

--- project.c
+++ project.c
@@ -9,76 +9,89 @@
 //Handle the case of missing one of the parentheses (in the parentheses' condition)
 
 
-typedef struct {
+typedef struct
+{
     float data[MAX];
-    struct Node *next;
+    struct Node* next;
 } Node;
 
-typedef struct {
-    Node *top;
+typedef struct
+{
+    Node* top;
 } Stack;
 
-Stack *initialize() {
-    Stack *s = malloc(sizeof(Stack));
+Stack* initialize()
+{
+    Stack* s = malloc(sizeof(Stack));
     s->top = NULL;
     return s;
 }
 
-float pop(Stack *s) {
-    Node *temp = s->top;
+float pop(Stack* s)
+{
+    Node* temp = s->top;
     float value = strtof(temp->data, NULL);
     s->top = temp->next;
     free(temp);
     return value;
 }
 
-void push(Stack *s, float value) {
-    Node *n = malloc(sizeof(Node));
+void push(Stack* s, float value)
+{
+    Node* n = malloc(sizeof(Node));
     sprintf(n->data, "%f", value);
     n->next = s->top;
     s->top = n;
 }
 
-float peek(Stack *s) {
+float peek(Stack* s)
+{
     return strtof(s->top->data, NULL);
 }
 
-int isEmpty(Stack *s) {
+int isEmpty(Stack* s)
+{
     return s->top ? 0 : 1;
 }
 
 
-int precedence(char c) {
-    switch (c) {
-        case '^':
-            return 3;
-        case '*':
-        case '/':
-        case '%':
-            return 2;
-        case '+':
-        case '-':
-            return 1;
-        default:
-            return 0;
+int precedence(char c)
+{
+    switch (c)
+    {
+    case '^':
+        return 3;
+    case '*':
+    case '/':
+    case '%':
+        return 2;
+    case '+':
+    case '-':
+        return 1;
+    default:
+        return 0;
     }
 }
 
 // Infix to postfix conversion function
-char *infixToPostfix(char *infix) {
-    char *postfix = malloc(
-            strlen(infix) + 2); // Allocate enough space for postfix expression (including null terminator)
-    Stack *s = initialize();
+char* infixToPostfix(char* infix)
+{
+    char* postfix = malloc(
+        strlen(infix) + 2); // Allocate enough space for postfix expression (including null terminator)
+    Stack* s = initialize();
 
     int i, j = 0;
     char ch;
 
-    for (i = 0; i < strlen(infix); i++) {
-        if (infix[i] == '-' && isdigit(infix[i + 1])) {
+    for (i = 0; i < strlen(infix); i++)
+    {
+        if (infix[i] == '-' && isdigit(infix[i + 1]))
+        {
             postfix[j++] = '-';
             i++;
         }
-        while (isdigit(infix[i]) || infix[i] == '.') {
+        while (isdigit(infix[i]) || infix[i] == '.')
+        {
             postfix[j++] = infix[i];
             i++;
         }
@@ -88,19 +101,27 @@ char *infixToPostfix(char *infix) {
         if (postfix[j - 1] != ' ')
             postfix[j++] = ' ';
 
-        if (infix[i] == '(') {
+        if (infix[i] == '(')
+        {
             push(s, infix[i]);
-        } else if (infix[i] == ')') {
-            while ((ch = pop(s)) != '(' && !isEmpty(s)) {
+        }
+        else if (infix[i] == ')')
+        {
+            while ((ch = pop(s)) != '(' && !isEmpty(s))
+            {
                 postfix[j++] = ch;
                 postfix[j++] = ' ';
             }
-//            if (ch == '(') {
-//                printf("Invalid expression: Missing closing parenthesis\n");
-//                exit(1);
-//            }
-        } else {  // Operator (+, -, *, /, %, ^)
-            while (!isEmpty(s) && precedence(peek(s)) >= precedence(infix[i])) {
+            //            if (ch == '(') {
+            //                printf("Invalid expression: Missing closing parenthesis\n");
+            //                exit(1);
+            //            }
+        }
+        else
+        {
+            // Operator (+, -, *, /, %, ^)
+            while (!isEmpty(s) && precedence(peek(s)) >= precedence(infix[i]))
+            {
                 postfix[j++] = pop(s);
                 postfix[j++] = ' ';
             }
@@ -109,18 +130,20 @@ char *infixToPostfix(char *infix) {
     }
 
     // Pop remaining operators from the stack
-    while (!isEmpty(s)) {
+    while (!isEmpty(s))
+    {
         postfix[j++] = pop(s);
         postfix[j++] = ' ';
     }
     postfix[j] = '\0'; // Add null terminator
 
-    free(s);  // Free memory allocated for the stack
+    free(s); // Free memory allocated for the stack
     return postfix;
 }
 
 
-int main(void) {
+int main(void)
+{
     char infix[MAX], *postfix;
     printf("Please enter the infix expression:\n");
     fgets(infix, MAX, stdin);
